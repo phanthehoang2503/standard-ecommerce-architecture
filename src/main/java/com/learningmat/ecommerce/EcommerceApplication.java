ed650526b12ee707b258a350d43116a5f0feb8f2
The Hoang <phanthehoang2503@gmail.com>
2026-03-16 16:58:00 +0700

    chore(config): update project dependencies and main application class

--- src/main/java/com/learningmat/ecommerce/EcommerceApplication.java
+++ src/main/java/com/learningmat/ecommerce/EcommerceApplication.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,7 @@ import com.learningmat.ecommerce.module.user.UserRepository;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
